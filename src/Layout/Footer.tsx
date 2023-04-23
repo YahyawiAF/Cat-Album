50102c910f651291f7a8d28b28d748ab69d1e79e
yahyawiaf <abdelftt@gmail.com>
2023-04-23 15:45:16 +0200

    layout

---
+++ src/Layout/Footer.tsx
@@ -0,0 +1,12 @@
+export const Footer = () => {
+  return (
+    <footer className="text-muted py-5">
+      <div className="container">
+        <p className="float-right">
+          <a href="#">Back to top</a>
+        </p>
+        <p>&copy; Yahyawi Abdel fatteh</p>
+      </div>
+    </footer>
+  );
+};
